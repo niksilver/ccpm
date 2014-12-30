e6b7017f39c9bfe367c89d0269aafb378122eb10
Nik Silver <nik@niksilver.com>
2014-12-30 14:32:32 +0000

    Separated the Plan DSL verbs from the Plan class

--- src/main/scala/org/pigsaw/ccpm/Plan.scala
+++ src/main/scala/org/pigsaw/ccpm/Plan.scala
@@ -5,16 +5,21 @@ import scala.collection.mutable.MutableList
 /**
  * A project plan, which may or may not be a good one.
  */
-class Plan {
-
-  private val taskList = MutableList[Task]()
+class Plan extends PlanVerbs {
+  
+  val taskList = MutableList[Task]()
   def tasks: List[Task] = scala.collection.immutable.List(taskList: _*)
 
+}
+
+trait PlanVerbs {
+  this: Plan =>
+    
   object add {
     def task(desc: String) = {
       val t = Task(desc)
       taskList += t
     }
   }
+  
 }
-
