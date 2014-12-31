66fd8769b24dc6219387701e28058f937e3493e7
Nik Silver <nik@niksilver.com>
2014-12-31 23:00:46 +0000

    Moved some code into their own source files.

---
+++ src/main/scala/org/pigsaw/ccpm/Exceptions.scala
@@ -0,0 +1,7 @@
+package org.pigsaw.ccpm
+
+class ProjectException(msg: String) extends Exception(msg)
+
+class DuplicateTaskException(msg: String) extends ProjectException(msg)
+class UnknownTaskException(msg: String) extends ProjectException(msg)
+class CyclicDependencyException(msg: String) extends ProjectException(msg)
