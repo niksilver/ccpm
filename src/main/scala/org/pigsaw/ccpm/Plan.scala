d30b6525742b8808b7fc8b82b94b991c2bc659d7
Nik Silver <nik@niksilver.com>
2015-01-02 23:17:34 +0000

    Enforced functional nature of Plan class, so it no longer relies on the mutability of subclass fields

--- src/main/scala/org/pigsaw/ccpm/Plan.scala
+++ src/main/scala/org/pigsaw/ccpm/Plan.scala
@@ -8,7 +8,7 @@ class Plan extends PlanVerbs {
   /**
    * Tasks in the plan, in the order in which they were created.
    */
-  def tasks: List[Task] = scala.collection.immutable.List(taskList: _*)
+  lazy val tasks: List[Task] = scala.collection.immutable.List(taskList: _*)
 
   def task(id: Symbol) = {
     val tList = taskList filter { _.id == id }
@@ -21,11 +21,11 @@ class Plan extends PlanVerbs {
   /**
    * Resources in the plan, in the order in which they were declared
    */
-  def resources: Seq[String] = scala.collection.immutable.List(resourcesList: _*)
+  lazy val resources: Seq[String] = scala.collection.immutable.List(resourcesList: _*)
 
   /**
    * A list of task pairs `t0 -> t1` where `t0` has to finish
    * before `t1` can start.
    */
-  def dependencies: List[(Task, Task)] = scala.collection.immutable.List(dependenciesList: _*)
+  lazy val dependencies: List[(Task, Task)] = scala.collection.immutable.List(dependenciesList: _*)
 }
