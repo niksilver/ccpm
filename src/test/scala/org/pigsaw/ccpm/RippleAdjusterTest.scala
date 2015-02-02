99ada97c91366e69e3a3e36bddfa30553425d82e
Nik Silver <nik@niksilver.com>
2015-02-02 21:36:18 +0000

    The problem set for the RippleAdjuster is getting more complex

--- src/test/scala/org/pigsaw/ccpm/RippleAdjusterTest.scala
+++ src/test/scala/org/pigsaw/ccpm/RippleAdjusterTest.scala
@@ -5,36 +5,42 @@ import org.scalatest.Matchers
 
 class RippleAdjusterTest extends FlatSpec with Matchers {
   // The "linear shunt" problem is this:
-  // There is a string of "x"s and "."s. An "x" represents a piece, and
-  // a "." represents a space. We want to move an "x" along the string
+  // There is a string of letters and "."s. A letter represents a piece, and
+  // a "." represents a space. We want to move an letter along the string
   // a given number of places rightwards. But it can only move into a space.
-  // However, we can move another "x" along if it's in the way.
-  // No "x" can move beyond the end of the string.
-  
+  // However, we can move another letter along if it's in the way.
+  // No letter can move beyond the end of the string.
+
   /**
-   * Description of the desire to move the "x" at index `index`
+   * Description of the desire to move the letter at index `index`
    * a given number of `steps`.
    */
   case class LinShMove(index: Int, steps: Int)
-  
+
   /**
    * A `RippleAdjuster` describing the linear shunt problem.
    */
   class LinShRippleAdjuster(board: String) extends RippleAdjuster[LinShMove](board) {
     def attempt(m: LinShMove) = {
-      board.updated(m.index, ".").updated(m.index + 1, "x").mkString
+      board.updated(m.index, ".").updated(m.index + m.steps, "x").mkString
     }
   }
-  
+
   "solve" should "solve a simple one-step problem (1)" in {
     val ra = new LinShRippleAdjuster("x.")
     val move = LinShMove(0, 1)
     ra.solve(move) should equal (".x")
   }
-  
+
   it should "solve a simple one-step problem (2 - to avoid faking)" in {
     val ra = new LinShRippleAdjuster("x..")
     val move = LinShMove(0, 1)
     ra.solve(move) should equal (".x.")
+  }
+
+  it should "solve a simple one-step problem with a different kind of move" in {
+    val ra = new LinShRippleAdjuster("x...")
+    val move = LinShMove(0, 2)
+    ra.solve(move) should equal ("..x.")
   }
 }
