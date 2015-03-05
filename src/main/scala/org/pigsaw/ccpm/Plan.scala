d191279a120752ba6c4c0caebd344b518f82a81d
Nik Silver <nik@niksilver.com>
2015-03-05 23:33:48 +0000

    Removed unnecessary methods: Plan.nonCriticalPaths and Plan.exclusiveFeederPaths

--- src/main/scala/org/pigsaw/ccpm/Plan.scala
+++ src/main/scala/org/pigsaw/ccpm/Plan.scala
@@ -91,72 +91,12 @@ trait Plan {
   def isOnCriticalChain(t: Task): Boolean = { criticalChain contains t }
 
   /**
-   * Get all paths that are distinct from the critical chain.
-   * Each task will be on exactly one path, and none of the
-   * tasks will be on the critical chain.
-   * That means that all the tasks in all the non-critical
-   * paths and the critical chain combined is exactly all
-   * the tasks in the plan, with no repeats.
-   */
-  def nonCriticalPaths: Set[Seq[Task]] = {
-    buildSlices(graph.paths, Set(), criticalChain.toSet)
-  }
-
-  /**
-   * Given some paths, return the slices found from removing
-   * all the excluded tasks. Initially the excluded tasks are
-   * those on the critical chain, but as more slices are
-   * found so the tasks on those slices are added to the excluded
-   * list.
-   */
-  private def buildSlices(paths: Set[Seq[Task]], acc: Set[Seq[Task]], excluded: Set[Task]): Set[Seq[Task]] = {
-    paths.size match {
-      case 0 => acc
-      case _ => {
-        val (path, rest) = paths.splitAt(1)
-        val (newAcc, newExcluded) = slicePath(path.head, acc, excluded)
-        buildSlices(rest, newAcc, newExcluded)
-      }
-    }
-  }
-
-  /**
-   * Given a path, return the slices found from
-   * removing all the `excluded` tasks.
-   * @param path  The path to slice.
-   * @param acc   The accumulated slices found so far
-   * @excluded    Tasks to exclude, to leave the slices
-   * @returns   A pair: The slices, and the excluded tasks (which are
-   *            an amalgam of the tasks in the slices)
-   */
-  private def slicePath(path: Seq[Task], acc: Set[Seq[Task]], excluded: Set[Task]): (Set[Seq[Task]], Set[Task]) = {
-    if (path.isEmpty) {
-      (acc, excluded)
-    } else {
-      val usable = path dropWhile { excluded contains _ }
-      val (slice, next) = usable span { t => !(excluded contains t) }
-      val newAcc = if (slice.isEmpty) acc else acc + slice
-      slicePath(next, newAcc, slice ++: excluded)
-    }
-  }
-
-  /**
    * Get the task that follows on from this path and which
    * is on the critical chain
    */
   def feedOnCriticalChain(path: Seq[Task]): Option[Task] = {
     val nextTasks = graph.successors(path.last)
     nextTasks find { isOnCriticalChain(_) }
-  }
-
-  /**
-   * Get paths which feed directly into the critical chain.
-   * No task is repeated in the result, so if there are two
-   * overlapping paths which feed into the critical chain
-   * only one of them will be returned here.
-   */
-  def exclusiveFeederPaths: Set[Seq[Task]] = {
-    nonCriticalPaths filter { feedOnCriticalChain(_).nonEmpty }
   }
 
   /**
