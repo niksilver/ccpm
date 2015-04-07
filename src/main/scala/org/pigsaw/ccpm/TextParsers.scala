0f8e4c8005aaf9a1c2685d6c02bd8ed3d50fd61e
Nik Silver <nik@niksilver.com>
2015-04-07 17:46:13 +0100

    Is now parsing durations

--- src/main/scala/org/pigsaw/ccpm/TextParsers.scala
+++ src/main/scala/org/pigsaw/ccpm/TextParsers.scala
@@ -3,13 +3,19 @@ package org.pigsaw.ccpm
 import scala.util.parsing.combinator.RegexParsers
 
 /**
- * Parse a piece of text to create a `Plan`.
+ * Parse a piece of text to create a `Plan`. Define a task like this:
+ * 
+ * {{{
+ *     t1: "My first task"
+ * }}}
  */
 class TextParser extends RegexParsers {
 
   def taskID: Parser[Symbol] = "[a-zA-Z][0-9a-zA-Z]*".r ^^ { Symbol(_) }
   
   def taskDescription: Parser[String] = "\"" ~> """[^"]*""".r <~ "\"" ^^ { _.toString }
+  
+  def duration: Parser[Double] = ("""[0-9]*\.[0-9]+""".r | """[0-9]+""".r) ^^ { _.toDouble }
   
   def taskLine: Parser[Task] = taskID ~ ":" ~ taskDescription ^^ { case (id ~ ":" ~ desc) => Task(id, desc) }
 }
--- src/test/scala/org/pigsaw/ccpm/TextParsersTest.scala
+++ src/test/scala/org/pigsaw/ccpm/TextParsersTest.scala
@@ -66,6 +66,57 @@ class TextParsersTest extends FlatSpec with Matchers {
     }
   }
   
+  "duration" should "parse some digits" in {
+    new TextParser with ParserMatchers {
+      parseAll(duration, "1234") should parseAs (1234.0)
+      parseAll(duration, "9") should parseAs (9.0)
+    }
+  }
+  
+  it should "match digits dot and digits" in {
+    new TextParser with ParserMatchers {
+      parseAll(duration, "1234.56") should parseAs (1234.56)
+      parseAll(duration, "9.5") should parseAs (9.5)
+    }
+  }
+  
+  it should "match just a dot and then digits" in {
+    new TextParser with ParserMatchers {
+      parseAll(duration, ".56") should parseAs (0.56)
+      parseAll(duration, ".5") should parseAs (0.5)
+    }
+  }
+  
+  it should "reject no digits after the decimal point" in {
+    new TextParser with ParserMatchers {
+      parseAll(duration, "1234.") shouldNot parseOkay
+      parseAll(duration, "9.") shouldNot parseOkay
+    }
+  }
+  
+  it should "reject just a decimal point" in {
+    new TextParser with ParserMatchers {
+      parseAll(duration, ".") shouldNot parseOkay
+    }
+  }
+  
+  it should "reject things with letters in" in {
+    new TextParser with ParserMatchers {
+      parseAll(duration, "123A.56") shouldNot parseOkay
+      parseAll(duration, "1234.5X") shouldNot parseOkay
+    }
+  }
+  
+  it should "handle very long numbers okay" in {
+    new TextParser with ParserMatchers {
+      val num = "123456" + ("0" * 15)
+      val out = parseAll(duration, num)
+      out should parseOkay
+      out.get shouldBe > (123455e15)
+      out.get shouldBe < (123457e15)
+    }
+  }
+  
   "taskLine" should "parse a task ID and description (1)" in {
     new TextParser with ParserMatchers {
       parseAll(taskLine, """t1: "My first task"""") should parseAs (Task('t1, "My first task"))
